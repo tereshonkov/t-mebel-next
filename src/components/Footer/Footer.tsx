e572430c0bfbf03ecd30c664c74bc4e572fc4d6f
tereshonkov <tereshonkov.dima@gmail.com>
2025-05-12 09:37:49 +0300

    feat: Enhance Footer component with internationalization support

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,33 @@
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import { getTranslations } from "next-intl/server";
 
-export default function Footer() {
+export default async function Footer() {
+  const t = await getTranslations("Footer");
   return (
     <footer className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.logo}>
           <img src="/logo.png" alt="Logo" />
-          <h3>Мебель под заказ — удобно. стильно. индивидуально.</h3>
+          <h3>{t('logo-text')}</h3>
         </div>
         <nav className={styles.navFooter}>
           <ul className={styles.navContainer}>
-            <Link href="#">Главная</Link>
-            <Link href="#">Портфолио</Link>
-            <Link href="#">О Компании</Link>
-            <Link href="#">Блог</Link>
-            <Link href="#">Отзывы клиентов</Link>
-            <Link href="#">Контакты</Link>
+            <Link href="#">{t('home')}</Link>
+            <Link href="#">{t('portfolio')}</Link>
+            <Link href="#">{t('about')}</Link>
+            <Link href="#">{t('blog')}</Link>
+            <Link href="#">{t('reviews')}</Link>
+            <Link href="#">{t('contacts')}</Link>
           </ul>
         </nav>
         <div className={styles.contacts}>
           <div className={styles.phone}>
-          <p>Пн-Сб с 9:00 до 19:00</p>
-            <Link href="tel:0671496741">Телефон: (067)-149-67-41</Link>
+          <p>{t('days')}</p>
+            <Link href="tel:0671496741">{t('phone')}</Link>
           </div>
           <div className={styles.address}>
-            <p>г. Харьков, ул. Льва Ландау 8</p>
+            <p>{t('adress')}</p>
           </div>
         </div>
         <div className={styles.social}>
@@ -41,9 +43,9 @@ export default function Footer() {
         </div>
       </div>
       <div className={styles.copyright}>
-        <p>© 2023 Мебель на заказ t-mebel. Все права защищены.</p>
-        <Link className={styles.link} href="#">Политика конфиденциальности</Link>
-        <Link className={styles.link} href="#">Пользовательское соглашение</Link>
+        <p>{t('name')}.</p>
+        <Link className={styles.link} href="#">{t('privacy')}</Link>
+        <Link className={styles.link} href="#">{t('user-agreement')}</Link>
       </div>
     </footer>
   );
