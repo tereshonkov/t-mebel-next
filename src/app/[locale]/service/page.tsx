55a9a3a3577de01859aedf413df37ef3c5b30ad2
tereshonkov <tereshonkov.dima@gmail.com>
2025-05-14 15:53:15 +0300

    feat: Add BlockService component with layout and styling

--- src/app/[locale]/service/page.tsx
+++ src/app/[locale]/service/page.tsx
@@ -1,15 +1,24 @@
 import HeaderServer from "@/components/Header/Header-server"
 import Footer from "@/components/Footer/Footer"
 import ServiceHeader from "@/components/ServiceHeader/ServiceHeader"
+import BlockService from "@/components/BlockService/BlockService"
+
+
+
 
 export default function page() {
   return (
-    <div className="container">
-      <HeaderServer />
-    <main className="main-service">
-    <ServiceHeader />
-    </main>
-    <Footer />
-    </div>
+    <>
+
+
+      <div className="container">
+        <HeaderServer />
+        <main className="main-service">
+          <ServiceHeader />
+          <BlockService />
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
---
+++ src/components/BlockService/BlockService.tsx
@@ -0,0 +1,31 @@
+import styles from './BlockService.module.css';
+import Button from '../Button/Button';
+
+export default function BlockService() {
+  return (
+    <section className={styles.wrapper}>
+        <div className={styles.sidebar}>
+            <h3>Выберите категорию</h3>
+            <ul className={styles.lists}>
+                <li>Кухни</li>
+                <li>Шкафы</li>
+                <li>Спальня</li>
+                <li>Ванные комнаты</li>
+                <li>Магазины</li>
+            </ul>
+        </div>
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <div className={styles.image}>
+                    <img src="https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/1/tablet.webp" alt="" />
+                </div>
+                <div className={styles.text}>
+                    <h3>Кухня серая</h3>
+                    <p>Короткое описание</p>
+                    <Button dark link="#">Заказать</Button>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
