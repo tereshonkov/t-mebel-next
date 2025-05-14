ec118bf0d9c20adf56c12ff5e076012db48fa786
tereshonkov <tereshonkov.dima@gmail.com>
2025-05-14 09:17:39 +0300

    feat: Remove HeroService and Portfolio components along with their styles

--- src/app/[locale]/service/page.tsx
+++ src/app/[locale]/service/page.tsx
@@ -1,7 +1,6 @@
 import HeaderServer from "@/components/Header/Header-server"
 import Footer from "@/components/Footer/Footer"
 import ServiceHeader from "@/components/ServiceHeader/ServiceHeader"
-import Portfolio from "@/components/Portfolio/Portfolio"
 
 export default function page() {
   return (
@@ -9,8 +8,6 @@ export default function page() {
       <HeaderServer />
     <main className="main-service">
     <ServiceHeader />
-    {/* <HeroService /> */}
-    <Portfolio />
     </main>
     <Footer />
     </div>
