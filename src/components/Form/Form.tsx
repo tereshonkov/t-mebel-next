8ecf6e135eb7e4abb6af2949e7ea00a6d0c985c8
tereshonkov <tereshonkov.dima@gmail.com>
2025-05-31 07:26:24 -0700

    refactor: Update Form component styles, align form elements, and integrate phone number display

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,20 @@
-import styles from './Form.module.css';
-import FormClient from './FormClient';
+import styles from "./Form.module.css";
+import { Link } from "@/i18n/navigation";
 
 export default function Form() {
-    return (
-        <section className={styles.wrapper}>
-            <h2 className={styles.title}>Получите <span>бесплатную</span> консультацию уже <span>сегодня</span></h2>
-            <FormClient />
-        </section>
-    )
+  return (
+    <section className={styles.wrapper}>
+      <h2 className={styles.title}>
+        Получите <span>бесплатную</span> консультацию уже <span>сегодня</span>
+      </h2>
+      <div className={styles.numberWrapper}>
+        <h3 className={styles.number}>
+            067 - 149 - 67 - 41
+        </h3>
+        <Link href="tel:0671496741" className={styles.btn}>
+          Позвонить нам
+        </Link>
+      </div>
+    </section>
+  );
 }
--- src/components/Form/FormClient.tsx
+++ src/components/Form/FormClient.tsx
@@ -5,9 +5,6 @@ import { useState } from 'react';
 export default function FormClient() {
     const [value, setValue] = useState("");
   return (
-    <form className={styles.form}>
-            <input onChange={(e) => {setValue(e.target.value)}} placeholder='067 - 149 - 67 - 41'  name='phone' value={value} type="phone" className={styles.input} />
-            <button className={styles.btn} type='submit'>Позвонить мне</button>
-    </form>
+
   )
 }
