148c54ae14ea6e4deae94dbf5336e5b2777f5cc6
tereshonkov <tereshonkov.dima@gmail.com>
2025-06-02 14:45:43 +0300

    style: Adjust CSS cursor property in Language component; comment out for clarity and maintain layout consistency

--- src/components/Language/Language.tsx
+++ src/components/Language/Language.tsx
@@ -4,28 +4,28 @@ import { useTranslations } from 'next-intl';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 export default function Language({ mobile = false }: { mobile?: boolean }) {
-    
+
     const t = useTranslations('header');
     const router = useRouter();
     const pathname = usePathname();
     const searchParams = useSearchParams();
-    const currentLocale = pathname.split('/')[1]; 
+    const currentLocale = pathname.split('/')[1];
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    // Сохраняем выбранный язык
-    const selectedLocale = e.target.value;
+        // Сохраняем выбранный язык
+        const selectedLocale = e.target.value;
 
-    // Формируем строку с query параметрами, если они есть
-    const paramsString = searchParams.toString();
-    const query = paramsString ? `?${paramsString}` : '';
+        // Формируем строку с query параметрами, если они есть
+        const paramsString = searchParams.toString();
+        const query = paramsString ? `?${paramsString}` : '';
 
-    // Формируем новый URL, заменяя старую локаль на новую
-    // Например: "/ru/products" -> "/en/products"
-    const newPathname = pathname.replace(`/${currentLocale}`, `/${selectedLocale}`);
+        // Формируем новый URL, заменяя старую локаль на новую
+        // Например: "/ru/products" -> "/en/products"
+        const newPathname = pathname.replace(`/${currentLocale}`, `/${selectedLocale}`);
 
-    // Делаем переход на новый URL с помощью роутера Next.js
-    router.push(`${newPathname}${query}`);
-  };
+        // Делаем переход на новый URL с помощью роутера Next.js
+        router.push(`${newPathname}${query}`);
+    };
     return (
         <div className={mobile ? styles.langWrapperDark : styles.langWrapper}>
             <img src={!mobile ? "/lang.svg" : "/lang-dark.svg"} alt="language" />
@@ -35,7 +35,7 @@ export default function Language({ mobile = false }: { mobile?: boolean }) {
                     <option className={styles.darkItem} value="en">English</option>
                     <option className={styles.darkItem} value="ru">Русский</option>
                     <option className={styles.darkItem} value="uk">Українська</option>
-                </select>    
+                </select>
             </div>
         </div>
     )
