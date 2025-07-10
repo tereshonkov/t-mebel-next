e2a7a9f417c0cf034be366e9523689d4ec105159
tereshonkov <tereshonkov.dima@gmail.com>
2025-07-10 15:09:37 +0300

    feat: Refactor FurnitureClient and useDataFurniture components; integrate dynamic data fetching and update image properties

--- src/components/Furniture/FurnitureClient.tsx
+++ src/components/Furniture/FurnitureClient.tsx
@@ -1,12 +1,10 @@
 "use client";
 import styles from "./Furniture.module.css";
-import { useState } from "react";
 import useDataFurniture from "./useDataFurniture";
 import { Link } from '@/i18n/navigation';
 
 export default function FurnitureClient({ limit }: { limit: number}) {
-    const [active, setActive] = useState<number>(1);
-    const { selected, kitchens, wardrobe, shops, bedrooms } = useDataFurniture();
+    const { selected, active, setActive, kitchens, wardrobe, bedrooms, store } = useDataFurniture();
   return (
     <div className={styles.container}>
     <ul className={styles.nav}>
@@ -26,24 +24,24 @@ export default function FurnitureClient({ limit }: { limit: number}) {
       {active === 1 &&
         kitchens.slice(0, limit).map((item, index) => (
           <Link href={`/service/${item.id}`} className={styles.card} key={item.id || index}>
-            <img src={item.img} alt={`Кухня ${index + 1}`} />
+            <img src={item.image} alt={`Кухня ${index + 1}`} />
           </Link>
         ))}
       {active === 2 &&
         wardrobe.slice(0, limit).map((item, index) => (
           <Link href={`/service/${item.id}`} className={styles.card} key={item.id || index}>
-            <img src={item.img} alt={`Шкаф ${index + 1}`} />
+            <img src={item.image} alt={`Шкаф ${index + 1}`} />
           </Link>
         ))}
         {active === 3 &&
-        shops.slice(0, limit).map((item, index) => (
+        store.slice(0, limit).map((item, index) => (
           <Link href={`/service/${item.id}`} className={styles.card} key={item.id ||index}>
-            <img src={item.img} alt={`Магазин ${index + 1}`} />
+            <img src={item.image} alt={`Магазин ${index + 1}`} />
           </Link>
         ))}
         {active === 4 && bedrooms.slice(0, limit).map((item, index) => (
           <Link href={`/service/${item.id}`} className={styles.card} key={item.id || index}>
-            <img src={item.img} alt={`Спальня ${index + 1}`} />
+            <img src={item.image} alt={`Спальня ${index + 1}`} />
           </Link>
         ))}
     </div>
--- src/components/Furniture/useDataFurniture.tsx
+++ src/components/Furniture/useDataFurniture.tsx
@@ -1,13 +1,23 @@
 import { useTranslations } from "next-intl";
+import { useState, useEffect } from "react";
 
 
 export default function useDataFurniture() {
 
   const t = useTranslations("furniture");
 
-  type Service = {
+  type Data = {
     id: number;
-    img: string;
+    title: string;
+    description: string;
+    color: string;
+    furnitures: string;
+    image: string;
+    images?: string[];
+    width?: number;
+    height?: number;
+    raiting?: number;
+    categories?: string[];
   };
   type SelectedItem = {
     name: string;
@@ -32,97 +42,120 @@ export default function useDataFurniture() {
     },
   ];
 
-  const kitchens: Service[] = [
-    {
-      id: 1,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/1/tablet.webp",
-    },
-    {
-      id: 2,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/2/tablet.webp",
-    },
-    {
-      id: 3,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/3/tablet.webp",
-    },
-    {
-      id: 4,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/4/tablet.webp",
-    },
-    {
-      id: 5,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/7/tablet.webp",
-    },
-    {
-      id: 6,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/5384567338008963271.webp",
-    },
-  ];
-  const wardrobe: Service[] = [
-    {
-      id: 7,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/1/tablet.webp",
-    },
-    {
-      id: 8,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/2/tablet.webp",
-    },
-    {
-      id: 9,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/3/tablet.webp",
-    },
-    {
-      id: 10,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/4/tablet.webp",
-    },
-    {
-      id: 11,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/5/tablet.webp",
-    },
-    {
-      id: 12,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/7/tablet.webp",
-    },
-  ];
-  const shops: Service[] = [
-    {
-      id: 13,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/rizne/5/tablet.webp",
-    },
-  ]
-  const bedrooms: Service[] = [
-    {
-      id: 14,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/rizne/2/tablet.webp",
-    },
-    {
-      id: 15,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/8/tablet.webp"
-    },
-    {
-      id: 16,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/rizne/1/tablet.webp"
-    },
-    {
-      id: 17,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/modern/2/tablet.webp"
-    },
-    {
-      id: 18,
-      img: "https://storage.googleapis.com/t-mebel/Image/wall.jpg"
-    },
-    {
-      id: 19,
-      img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/1/tablet.webp",
-    },
-  ];
+  // const kitchens: Service[] = [
+  //   {
+  //     id: 1,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/1/tablet.webp",
+  //   },
+  //   {
+  //     id: 2,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/2/tablet.webp",
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/3/tablet.webp",
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/4/tablet.webp",
+  //   },
+  //   {
+  //     id: 5,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/7/tablet.webp",
+  //   },
+  //   {
+  //     id: 6,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/kitchen/5384567338008963271.webp",
+  //   },
+  // ];
+  // const wardrobe: Service[] = [
+  //   {
+  //     id: 7,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/1/tablet.webp",
+  //   },
+  //   {
+  //     id: 8,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/2/tablet.webp",
+  //   },
+  //   {
+  //     id: 9,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/3/tablet.webp",
+  //   },
+  //   {
+  //     id: 10,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/4/tablet.webp",
+  //   },
+  //   {
+  //     id: 11,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/5/tablet.webp",
+  //   },
+  //   {
+  //     id: 12,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/7/tablet.webp",
+  //   },
+  // ];
+  // const shops: Service[] = [
+  //   {
+  //     id: 13,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/rizne/5/tablet.webp",
+  //   },
+  // ]
+  // const bedrooms: Service[] = [
+  //   {
+  //     id: 14,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/rizne/2/tablet.webp",
+  //   },
+  //   {
+  //     id: 15,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/8/tablet.webp"
+  //   },
+  //   {
+  //     id: 16,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/rizne/1/tablet.webp"
+  //   },
+  //   {
+  //     id: 17,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/modern/2/tablet.webp"
+  //   },
+  //   {
+  //     id: 18,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/wall.jpg"
+  //   },
+  //   {
+  //     id: 19,
+  //     img: "https://storage.googleapis.com/t-mebel/Image/ourPage/modal/shafa/1/tablet.webp",
+  //   },
+  // ];
 
-
+  const [active, setActive] = useState<number>(1);
+  const [array, setArray] = useState<Data[]>([]);
+  useEffect(() => { 
+    const fetchData = async () => {
+      try{
+        const response = await fetch('/data/data.json');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log("Data fetched successfully in useDataFurniture component:", data);
+        setArray(data);
+      }catch (e) {
+        console.error("Error fetching data in useDataFurniture component!:", e);
+      }
+    }
+    fetchData();
+  }, []);
+  const kitchens = array.filter(item => item.categories?.includes('kitchen'));
+  const wardrobe = array.filter(item => item.categories?.includes('wardrobe'));
+  const store = array.filter(item => item.categories?.includes('store'));
+  const bedrooms = array.filter(item => item.categories?.includes('bedroom'));
   return {
+    active,
+    setActive,
+    selected,
     kitchens,
     wardrobe,
-    shops,
+    store,
     bedrooms,
-    selected,
   };
 }
