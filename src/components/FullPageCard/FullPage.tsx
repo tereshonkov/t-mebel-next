e6d91a6ea1961c257c22015fdd1891463a6fce3a
tereshonkov <tereshonkov.dima@gmail.com>
2025-07-11 12:49:37 -0700

    feat: add Data type definition and refactor FurniturePage component to use FullPageCard

---
+++ src/components/FullPageCard/FullPage.tsx
@@ -0,0 +1,122 @@
+"use client";
+import styles from "./FullPage.module.css";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+type Data = {
+    id: number;
+    title: string;
+    description: string;
+    color: string;
+    furnitures: string;
+    image: string;
+    images?: string[];
+    width?: number;
+    height?: number;
+    raiting?: number;
+    categories?: string[];
+  };
+
+export default function FullPage({ id }: { id: string }) {
+    const [data, setData] = useState<Data | null>(null);
+    console.log("FurniturePage data:", data);
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('/data/data.json');
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const items: Data[] = await response.json();
+          const item = items.find(item => item.id === parseInt(id));
+          if (item) {
+            setData(item);
+          } else {
+            console.error(`Item with id ${id} not found`);
+          }
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+      fetchData();
+    }, [id])
+  return (
+    <>
+      <section className={styles.wrapper}>
+        <div className={styles.slider}>
+          <div className={styles.sliderMain}>
+            <img
+              src={data?.image}
+              alt={data?.title}
+            />
+          </div>
+          <div className={styles.track}>
+            {data?.images && data.images.length > 0 ? (
+                data.images.map((image, index) => (
+              <div key={index} className={styles.trackItem}>
+                <img src={image} alt={`${data.title} ${index + 1}`} />
+              </div>
+            ))) : (
+                <div className={styles.trackItem}>
+                    <img src={data?.image} alt={data?.title} />
+                </div>
+            )}
+          </div>
+          <div className={styles.sliderBtns}>
+            <button className={styles.prev}></button>
+            <button className={styles.next}></button>
+          </div>
+        </div>
+        <div className={styles.description}>
+          <div className={styles.heading}>
+            <h2 className={styles.title}>
+              {data?.title}
+            </h2>
+            <div className={styles.stars}>
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+            </div>
+          </div>
+          <p className={styles.subtitle}>
+            {data?.description}
+          </p>
+          <div className={styles.colorDiv}>
+            <span className={styles.colorTitle}>Цвет:</span>
+            <div className={styles.colors} style={{backgroundColor: `${data?.color}`}}></div>
+          </div>
+          <p className={styles.width}>Размеры: {data?.width} x {data?.height}</p>
+          <p className={styles.furnitures}>Фурнитура: {data?.furnitures}</p>
+          <Link href="tel:0671496741" className={styles.link}>
+            По всем вопросам звонить: 067 - 149 - 67 - 41
+          </Link>
+        </div>
+      </section>
+      <section className={styles.reviews}>
+        <div className={styles.review}>
+          <div className={styles.reviewsHeading}>
+            <h3 className={styles.name}>Ivan Ivanov</h3>
+            <div className={styles.stars}>
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+              <img src="/star.svg" alt="star" />
+            </div>
+          </div>
+          <p className={styles.body}>
+            Очень удобная кровать — спать комфортно, механизм подъёма работает
+            плавно, а ниша внутри действительно вместительная. Отличный вариант,
+            особенно если нужно дополнительное место для хранения!
+          </p>
+        </div>
+        <div>
+          <button className={styles.btn}>Оставить отзыв</button>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/types/data.ts
@@ -0,0 +1,13 @@
+export type Data = {
+    id: number;
+    title: string;
+    description: string;
+    color: string;
+    furnitures: string;
+    image: string;
+    images?: string[];
+    width?: number;
+    height?: number;
+    raiting?: number;
+    categories?: string[];
+  };
