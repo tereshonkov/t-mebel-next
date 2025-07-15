0838ab924bd3bfe2ad16c849ff34637abefae1b3
tereshonkov <tereshonkov.dima@gmail.com>
2025-07-15 17:12:13 +0300

    feat: refactor FullPage component to integrate embla carousel for image slider functionality and enhance layout

--- src/components/FullPageCard/FullPage.tsx
+++ src/components/FullPageCard/FullPage.tsx
@@ -1,68 +1,88 @@
 "use client";
 import styles from "./FullPage.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Link from "next/link";
+import useEmblaCarousel from 'embla-carousel-react';
 
 type Data = {
-    id: number;
-    title: string;
-    description: string;
-    color: string;
-    furnitures: string;
-    image: string;
-    images: string[];
-    width?: number;
-    height?: number;
-    raiting?: number;
-    categories?: string[];
-  };
+  id: number;
+  title: string;
+  description: string;
+  color: string;
+  furnitures: string;
+  image: string;
+  images: string[];
+  width?: number;
+  height?: number;
+  raiting?: number;
+  categories?: string[];
+};
 
 export default function FullPage({ id }: { id: string }) {
-    const [data, setData] = useState<Data | null>(null);
-    console.log("FurniturePage data:", data);
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('/data/data.json');
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const items: Data[] = await response.json();
-          const item = items.find(item => item.id === parseInt(id));
-          if (item) {
-            setData(item);
-          } else {
-            console.error(`Item with id ${id} not found`);
-          }
-        } catch (error) {
-          console.error("Error fetching data:", error);
+  const [data, setData] = useState<Data | null>(null);
+  console.log("FurniturePage data:", data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data/data.json');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-      };
-      fetchData();
-    }, [id])
+        const items: Data[] = await response.json();
+        const item = items.find(item => item.id === parseInt(id));
+        if (item) {
+          setData(item);
+        } else {
+          console.error(`Item with id ${id} not found`);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [id])
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    align: 'start',
+    skipSnaps: false,
+    dragFree: true,
+    containScroll: 'trimSnaps',
+  });
+
+  const scrollPrev = useCallback(() => emblaApi && emblaApi.scrollPrev(), [emblaApi]);
+  const scrollNext = useCallback(() => emblaApi && emblaApi.scrollNext(), [emblaApi]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+  }, [emblaApi]);
   return (
     <>
       <section className={styles.wrapper}>
-        <div className={styles.slider}>
-          <div className={styles.sliderMain}>
-            <img
-              src={data?.image}
-              alt={data?.title}
-            />
+        <div className={styles.slider} >
+          <div className={styles.emblaViewport} ref={emblaRef}>
+            <div className={styles.emblaContainer}>
+              {data?.images?.map((img, index) => (
+                <div key={index} className={styles.sliderMain}>
+                  <img src={img} alt={data?.title} />
+                </div>
+              ))}
+            </div>
           </div>
+
           <div className={styles.track}>
-            {data?.images.map((image,index) => (
-                 <div key={index} className={styles.trackItem}>
-                 <img src={image} alt={data?.title} />
-             </div>
+            {data?.images.map((image, index) => (
+              <div onClick={() => emblaApi?.scrollTo(index)}
+                key={index} className={styles.trackItem}>
+                <img src={image} alt={data?.title} />
+              </div>
             ))}
-               
-            
           </div>
+
           <div className={styles.sliderBtns}>
-            <button className={styles.prev}></button>
-            <button className={styles.next}></button>
+            <button onClick={scrollPrev} className={styles.prev}></button>
+            <button onClick={scrollNext} className={styles.next}></button>
           </div>
         </div>
         <div className={styles.description}>
@@ -83,7 +103,7 @@ export default function FullPage({ id }: { id: string }) {
           </p>
           <div className={styles.colorDiv}>
             <span className={styles.colorTitle}>Цвет:</span>
-            <div className={styles.colors} style={{backgroundColor: `${data?.color}`}}></div>
+            <div className={styles.colors} style={{ backgroundColor: `${data?.color}` }}></div>
           </div>
           <p className={styles.width}>Размеры: {data?.width} x {data?.height}</p>
           <p className={styles.furnitures}>Фурнитура: {data?.furnitures}</p>
