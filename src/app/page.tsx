d384bf51d26ef30899f96a0da9a33a207d43cd8b
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-02 16:14:03 +0300

    style: format code for consistency and readability in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,55 @@
-import Hero from "@/components/Hero/Hero"
-import Service from "@/components/Service/Service"
-import Rewies from "@/components/Reviews/Reviews"
-import Logo from "@/components/Logo/Logo"
-import Furniture from "@/components/Furniture/Furniture"
-import Faq from "@/components/Faq/Faq"
-import Form from "@/components/Form/Form"
-import Footer from "@/components/Footer/Footer"
-import { Metadata } from "next"
-import { getTranslations } from 'next-intl/server';
+import Hero from "@/components/Hero/Hero";
+import Service from "@/components/Service/Service";
+import Rewies from "@/components/Reviews/Reviews";
+import Logo from "@/components/Logo/Logo";
+import Furniture from "@/components/Furniture/Furniture";
+import Faq from "@/components/Faq/Faq";
+import Form from "@/components/Form/Form";
+import Footer from "@/components/Footer/Footer";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const locale  = 'uk';
-  const t = await getTranslations({ locale, namespace: 'seoHome' });
+  const locale = "uk";
+  const t = await getTranslations({ locale, namespace: "seoHome" });
 
-    const baseUrl = 'https://t-mebel.com.ua';
+  const baseUrl = "https://t-mebel.com.ua";
 
   return {
-    title: t('title'),
-    description: t('description'),
+    title: t("title"),
+    description: t("description"),
     openGraph: {
-      title: t('title'),
-      description: t('description'),
-      url: baseUrl + '/',
-      siteName: 'T-Mebel',
+      title: t("title"),
+      description: t("description"),
+      url: baseUrl + "/",
+      siteName: "T-Mebel",
       locale,
     },
     alternates: {
-      canonical: baseUrl + '/',
+      canonical: baseUrl + "/",
       languages: {
-        uk: baseUrl + '/',
-        en: baseUrl + '/en',
-        ru: baseUrl + '/ru',
-        'x-default': baseUrl + '/',
+        uk: baseUrl + "/",
+        en: baseUrl + "/en",
+        ru: baseUrl + "/ru",
+        "x-default": baseUrl + "/",
       },
     },
   };
 }
 
-
 export default function page() {
   return (
     <div className="container">
-      <Hero startIndex={0} home={true}/>
-    <main>
-      <Service />
-      <Rewies />
-      <Logo />
-      <Furniture home={true}/>
-      <Faq />
-      <Form />
-    </main>
-    <Footer />
+      <Hero startIndex={0} home={true} />
+      <main>
+        <Service />
+        <Rewies />
+        <Logo />
+        <Furniture home={true} />
+        <Faq />
+        <Form />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
