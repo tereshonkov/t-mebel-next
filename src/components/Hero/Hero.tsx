7839742579efae6124cee86e1c9b03ee0ede6f34
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-09 00:45:26 -0700

    feat: remove PageVisit component from Hero and delete its implementation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,6 @@ import Header from '../Header/Header';
 import { Link } from '@/i18n/navigation';
 import HeroCarusel from './HeroCarusel';
 import { getTranslations } from 'next-intl/server';
-import PageVisit from '../PageVisit/PageVisit';
 
 
 export default async function Hero({ startIndex = 0, home, page }: { startIndex?: number, home?: boolean, page?:boolean }) {
@@ -11,7 +10,6 @@ export default async function Hero({ startIndex = 0, home, page }: { startIndex?
   const form = await getTranslations('form');
   return (
     <header className={styles.wrapper} style={page ? { height: '50vh' } : { height: '100vh' }}>
-      <PageVisit />
       <Header />
       <HeroCarusel initialIndex={startIndex} />
       {home && <div className={styles.wrapperContent}>
