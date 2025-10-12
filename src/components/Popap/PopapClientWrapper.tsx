3b0c6229f16a8949bfb93cbf356c1075bf9b1f07
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-12 13:12:26 -0700

    feat: use dynamic import for Popap component to disable server-side rendering

--- src/components/Popap/PopapClientWrapper.tsx
+++ src/components/Popap/PopapClientWrapper.tsx
@@ -1,5 +1,7 @@
 "use client";
-import Popap from "./Popap";
+import dynamic from "next/dynamic";
+
+const Popap = dynamic(() => import("./Popap"), { ssr: false });
 
 export default function PopapClientWrapper() {
   return <Popap />;
