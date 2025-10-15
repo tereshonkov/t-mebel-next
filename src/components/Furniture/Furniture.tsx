4e3ee610c860a200834b723fa6e448430fdb859e
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-15 04:44:40 -0700

    feat: hide link element in Furniture component for improved layout

--- src/components/Furniture/Furniture.tsx
+++ src/components/Furniture/Furniture.tsx
@@ -9,7 +9,7 @@ export default async function Slider({ home }: { home?: boolean }) {
     <section id="furniture" className={styles.wrapper}>
       {home && <h2 className={styles.title}>{t('title')}</h2>}
       <FurnitureClient limit={home ? 6 : 20} />
-      <div>
+      <div className={styles.link}>
         {home && <Button link="/service#furniture">{t('btnFurniture')}</Button>}
       </div>
     </section>
